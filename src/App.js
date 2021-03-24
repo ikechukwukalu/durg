4f1c0e965726895348cc77f36c9be625cb476d41
kalu ikechukwu e.a <swaykay.foreveralearner@gmail.com>
2021-03-24 20:53:00 +0100

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router  } from "react-router-dom";
+import { HashRouter as Router  } from "react-router-dom";
 import './App.css';
 import Components from './components/index.jsx';
 import ScrollToTop from './scroll.js';
@@ -9,14 +9,14 @@ class App extends Component {
   constructor() {
       super();
       this.state = {
-        base_url: 'http://provirtcomm.com/', // For production - Change to base directory folder name Eg. https://localhost/basename/ 
+        base_url: window.location.pathname, // For production - Change to base directory folder name Eg. https://localhost/basename/ 
         api_url: '' // Tha api url
       }
   }
 
   render() {
     return (
-        <Router basename={'/'}> {/* For production - Change to base directory folder name Eg. https://localhost/basename/  */}
+        <Router basename={'/durg'}> {/* For production - Change to base directory folder name Eg. https://localhost/basename/  */}
           <ScrollToTop>
             <Components base_url={this.state.base_url} api_url={this.state.api_url} />
           </ScrollToTop>
